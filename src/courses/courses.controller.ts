886c7f4503748265ce65f4f5e4b159bd293bd5a8
bielgennaro <zirondigabriel@gmail.com>
2023-07-12 19:28:24 -0300

    Aprofundando nos estudos com controllers

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
 
 @Controller('courses')
-export class CoursesController {}
+export class CoursesController {
+  @Get('list')
+  findAll(@Res() response): string {
+    return response.status(302).send('Lista de cursos');
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: number) {
+    return `Curso #${id}`;
+  }
+
+  @Post('/create')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  create(@Body('name') body: string) {
+    return body;
+  }
+}
