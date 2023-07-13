22f5e0840524d8d46ba11f3fae26e71ae88b1b0f
bielgennaro <zirondigabriel@gmail.com>
2023-07-13 01:50:36 -0300

    Adquirindo conhecimento sobre modules e services

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,29 +1,43 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
   Param,
+  Patch,
   Post,
-  Res,
 } from '@nestjs/common';
+import { CourseService } from './courses.service';
 
 @Controller('courses')
 export class CoursesController {
+  constructor(private readonly courseService: CourseService) {}
+
   @Get('list')
-  findAll(@Res() response): string {
-    return response.status(302).send('Lista de cursos');
+  findAll() {
+    return this.courseService.findAll();
   }
 
-  @Get('/:id')
-  findOne(@Param('id') id: number) {
-    return `Curso #${id}`;
+  @Get('unique/:id')
+  findOne(@Param('id') id: string) {
+    return this.courseService.findOne(id);
   }
 
-  @Post('/create')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  create(@Body('name') body: string) {
-    return body;
+  @Post('create')
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() body: string) {
+    return this.courseService.create(body);
+  }
+
+  @Patch('patch/:id')
+  update(@Param('id') id: string, @Body() body: string) {
+    return this.courseService.update(id, body);
+  }
+
+  @Delete('delete/:id')
+  remove(@Param('id') id: string) {
+    return this.courseService.remove(id);
   }
 }
---
+++ src/courses/courses.service.ts
@@ -0,0 +1,61 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Course } from './entities/course.entitiy';
+
+@Injectable()
+export class CourseService {
+  private courses: Course[] = [
+    {
+      id: 1,
+      name: 'Fundamentos do framework NestJS',
+      description: 'Fundamentos do framework NestJS',
+      tags: ['nodejs', 'nestjs', 'javascript', 'backend'],
+    },
+    {
+      id: 2,
+      name: 'Fundamentos do framework Angular',
+      description: 'Fundamentos do framework Angular',
+      tags: ['angular', 'javascript', 'frontend'],
+    },
+    {
+      id: 3,
+      name: 'Fundamentos do framework React',
+      description: 'Fundamentos do framework React',
+      tags: ['react', 'javascript', 'frontend'],
+    },
+  ];
+
+  findAll() {
+    return this.courses;
+  }
+
+  findOne(id: string) {
+    const course = this.courses.find((course) => course.id === Number(id));
+
+    if (!course) {
+      throw new HttpException(`Course ${id} not found`, HttpStatus.NOT_FOUND);
+    }
+    return course;
+  }
+
+  create(createCourseDto: any) {
+    this.courses.push(createCourseDto);
+  }
+
+  update(id: string, updateCourseDto: any) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    this.courses[indexCourse] = updateCourseDto;
+  }
+
+  remove(id: string) {
+    const indexCourse = this.courses.findIndex(
+      (course) => course.id === Number(id),
+    );
+
+    if (indexCourse >= 0) {
+      this.courses.splice(indexCourse, 1);
+    }
+  }
+}
