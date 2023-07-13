2e74e5cfcd8c621cc18f040c8a1ccec27c7be8f4
bielgennaro <zirondigabriel@gmail.com>
2023-07-13 04:26:38 -0300

    Criando entity e adicionando TypeORM

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -1,6 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesController } from './courses.controller';
 import { CourseService } from './courses.service';
+import { Course } from './entities/course.entitiy';
 
-@Module({ controllers: [CoursesController], providers: [CourseService] })
+@Module({
+  imports: [TypeOrmModule.forFeature([Course])],
+  controllers: [CoursesController],
+  providers: [CourseService],
+})
 export class CoursesModule {}
--- src/courses/courses.service.ts
+++ src/courses/courses.service.ts
@@ -1,62 +1,55 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCoursesDto } from './dto/create-courses.dto';
+import { UpdateCoursesDto } from './dto/update-courses.dto';
 import { Course } from './entities/course.entitiy';
 
 @Injectable()
 export class CourseService {
-  private courses: Course[] = [
-    {
-      id: 1,
-      name: 'Fundamentos do framework NestJS',
-      description: 'Fundamentos do framework NestJS',
-      tags: ['nodejs', 'nestjs', 'javascript', 'backend'],
-    },
-    {
-      id: 2,
-      name: 'Fundamentos do framework Angular',
-      description: 'Fundamentos do framework Angular',
-      tags: ['angular', 'javascript', 'frontend'],
-    },
-    {
-      id: 3,
-      name: 'Fundamentos do framework React',
-      description: 'Fundamentos do framework React',
-      tags: ['react', 'javascript', 'frontend'],
-    },
-  ];
+  constructor(
+    @InjectRepository(Course)
+    private readonly courseRepository: Repository<Course>,
+  ) {}
 
   findAll() {
-    return this.courses;
+    return this.courseRepository.find();
   }
 
-  findOne(id: string) {
-    const course = this.courses.find((course) => course.id === Number(id));
+  findOne(id: object) {
+    const course = this.courseRepository.findOne(id);
 
     if (!course) {
-      throw new HttpException(`Course ${id} not found`, HttpStatus.NOT_FOUND);
+      throw new NotFoundException(`Course ${id} not found`);
     }
     return course;
   }
 
-  create(createCourseDto: any) {
-    this.courses.push(createCourseDto);
-    return createCourseDto;
+  create(createCourseDto: CreateCoursesDto) {
+    const course = this.courseRepository.create(createCourseDto);
+    return this.courseRepository.save(course);
   }
 
-  update(id: string, updateCourseDto: any) {
-    const indexCourse = this.courses.findIndex(
-      (course) => course.id === Number(id),
-    );
+  async update(id: string, updateCourseDto: UpdateCoursesDto) {
+    const course = await this.courseRepository.preload({
+      id: +id,
+      ...updateCourseDto,
+    });
 
-    this.courses[indexCourse] = updateCourseDto;
-  }
-
-  remove(id: string) {
-    const indexCourse = this.courses.findIndex(
-      (course) => course.id === Number(id),
-    );
-
-    if (indexCourse >= 0) {
-      this.courses.splice(indexCourse, 1);
+    if (!course) {
+      throw new NotFoundException(`Course ${id} not found`);
     }
+
+    return this.courseRepository.save(course);
+  }
+
+  async remove(id: object) {
+    const course = await this.courseRepository.findOne(id);
+
+    if (!course) {
+      throw new NotFoundException(`Course ${id} not found`);
+    }
+
+    return this.courseRepository.remove(course);
   }
 }
